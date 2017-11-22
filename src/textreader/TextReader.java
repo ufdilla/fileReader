9d3b1b351b81ee225fd28c72a8050d4fc0a0f361
ufdilla <learnmore.cumi@gmail.com>
2017-11-22 15:25:14 +0700

    independency changed

--- src/textreader/TextReader.java
+++ src/textreader/TextReader.java
@@ -6,28 +6,31 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class TextReader {
+public class TextReader 
+{
 
- 
-    public static void main(String[] args) {
-     String data = "textfile.txt";
-     
-     try {
-         FileReader freader = new FileReader(data);
-         BufferedReader buffreader = new BufferedReader(freader);
-         
-         String text;
-         while ((text = buffreader.readLine()) != null)
-             System.out.println(text);
-     }
-     
-     catch (FileNotFoundException err) {
-         err.getMessage();
-     }
-     
-     catch (IOException ioe) {
+  public static void main(String[] args) 
+  {
+    String data = "textfile.txt";
+
+      try {
+          FileReader freader = new FileReader(data);
+          BufferedReader buffreader = new BufferedReader(freader);
+
+          String text;
+          while ((text = buffreader.readLine()) != null)
+              System.out.println(text);
+       }
+
+      catch (FileNotFoundException err) 
+        {
+            err.getMessage();
+        }
+
+      catch (IOException ioe) 
+        {
             ioe.getMessage();
         }
     }
-    
+
 }
